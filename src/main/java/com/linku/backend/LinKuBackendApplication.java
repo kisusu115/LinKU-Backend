f3448fd6faccd9dad01658fceff4627388fc2f7e
kisusu <kikisu115@naver.com>
2025-04-08 17:49:55 +0900

    feat: project setup

---
+++ src/main/java/com/linku/backend/LinKuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.linku.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinKuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinKuBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linku/backend/LinKuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linku.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinKuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
