883eed38a954fb4f5c9cf9e0effe89b2db5a02cf
kisusu <kikisu115@naver.com>
2025-04-12 22:10:52 +0900

    feat: ComponentScan Config 파일 추가 및 CI/CD 파이프라인 테스트용 컨트롤러 추가

---
+++ src/main/java/com/linku/backend/global/config/AutoWebConfig.java
@@ -0,0 +1,10 @@
+package com.linku.backend.global.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class AutoWebConfig {
+
+}
